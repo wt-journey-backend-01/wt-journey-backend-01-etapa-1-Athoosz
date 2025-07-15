26def513f46465c59b31946f7c74d8fe24a37da0
Athoosz <usacrabrasilathos@gmail.com>
2025-07-14 22:43:34 -0300

    fix:  add .gitignore for node_modules

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "views")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
